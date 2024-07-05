7a35fc520cae230c7f1cfc3b1b329bcad5e2b1a5
adranwit <adrianwit@gmail.com>
2024-07-05 15:50:19 -0700

    enhances qualify helper functions

--- insert/value.go
+++ insert/value.go
@@ -1,6 +1,9 @@
 package insert
 
-import "github.com/viant/sqlparser/node"
+import (
+	"github.com/viant/sqlparser/expr"
+	"github.com/viant/sqlparser/node"
+)
 
 type Value struct {
 	Expr     node.Node
@@ -8,4 +11,11 @@ type Value struct {
 	Raw      string
 	node.Span
 	Meta interface{}
+}
+
+func (v *Value) IsPlaceholder() bool {
+	return v.Raw == "?"
+}
+func (v *Value) Interface() (interface{}, error) {
+	return expr.NewValue(v.Raw)
 }
