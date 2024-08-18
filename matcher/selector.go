10491906740c14ba9c3d06c14bad4116a4c85bbf
adranwit <adrianwit@gmail.com>
2024-08-18 12:26:19 -0700

    added truncate

--- matcher/selector.go
+++ matcher/selector.go
@@ -42,7 +42,7 @@ func (n *selector) Match(cursor *parsly.Cursor) (matched int) {
 	}
 	for i := pos; i < size; i++ {
 		switch input[i] {
-		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '_', '.', ':', '$':
+		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '_', '.', ':', '$', '/':
 			matched++
 			continue
 		case '*':
